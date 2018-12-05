80bf4b6d59299eaf4c784796c1e68bf9f1639416
fdebrain <fdebrain@student.ethz.ch>
2018-12-05 20:00:49 +0100

    Added DLT algorithm

--- include/EBV_Stereo_Calibration.h
+++ include/EBV_Stereo_Calibration.h
@@ -13,11 +13,31 @@
 class IntrinsicsData
 {
 public:
-    cv::Mat_<double> m_K;
-    cv::Mat_<double> m_D;
+    cv::Mat_<float> m_K;
+    cv::Mat_<float> m_D;
     IntrinsicsData();
 };
 
+
+class DLT
+{
+public:
+    DLT();
+    ~DLT();
+    void normalizePoints(cv::Mat& points2D,
+                         cv::Mat& points3D);
+    void extractProjectionMatrix(const std::vector<cv::Point3d>& points3D,
+                                 const std::vector<cv::Point2d>& points2D,
+                                 cv::Mat& ctw, cv::Mat& cRw);
+
+    // Normalization matrices
+    cv::Mat m_U;
+    cv::Mat m_T;
+
+    //
+    cv::Mat m_R;
+    cv::Mat m_t;
+};
 
 class StereoCalibrator
 {
@@ -28,12 +48,12 @@ public:
                      Triangulator* triangulator=nullptr);
     ~StereoCalibrator();
 
-    void calibrateCameras(cv::Mat& frame, const uint id);
-    void calibrateLaser(cv::Mat& frame0, cv::Mat &frame1, const uint id);
+    void calibrateCameras(cv::Mat& frame, const int id);
+    void calibrateLaser(cv::Mat& frame0, cv::Mat &frame1, const int id);
     void saveCamerasCalibration();
     void saveLaserCalibration();
     const std::vector<cv::Point3f> calculateWorldPoints();
-    void pointLaserToPixel(const int u, const int v, const uint id);
+    void pointLaserToPixel(const int u, const int v, const int id);
 
     float getLearningRate() const { return m_learningRate; }
     void setLearningRate(const float lr) { m_learningRate=lr; }
@@ -51,8 +71,8 @@ public:
     const std::string m_path_calib_laser;
 
     // Intrinsics
-    const cv::Point2i m_pattern_size{8, 5};
-    const float m_pattern_square_size = 0.03f;
+    const cv::Point2i m_pattern_size{8,5};
+    const float m_pattern_square_size{0.03f};
     const size_t m_min_frames_to_capture{20};
     const int m_step{3};
     const std::chrono::milliseconds m_min_capture_delay{1000};
@@ -67,9 +87,7 @@ public:
 
     // Laser
     int m_threshConverged{2};
-    float m_learningRate;
-
-    std::mutex          m_laserMutex;
+    float m_learningRate{0.3f};
 };
 
 #endif // EBV_STEREO_CALIBRATION_H
--- src/EBV_Stereo_Calibration.cpp
+++ src/EBV_Stereo_Calibration.cpp
@@ -2,8 +2,8 @@
 
 IntrinsicsData::IntrinsicsData()
 {
-    m_K.resize(0);
-    m_D.resize(0);
+    m_K.resize(3*3);
+    m_D.resize(1*5);
 }
 
 StereoCalibrator::StereoCalibrator(LaserController* laser,
@@ -17,8 +17,7 @@ StereoCalibrator::StereoCalibrator(LaserController* laser,
       m_path_calib_laser(m_triangulator->m_path_calib_laser),
       m_last_frame_captured{std::chrono::high_resolution_clock::time_point{},
                             std::chrono::high_resolution_clock::time_point{}},
-      m_camera_intrinsics{IntrinsicsData{},IntrinsicsData{}},
-      m_learningRate(0.3f)
+      m_camera_intrinsics{IntrinsicsData{},IntrinsicsData{}}
 {
     for (auto& m: m_R) { m.resize(3*3); }
     for (auto& m: m_T) { m.resize(3*1); }
@@ -28,20 +27,18 @@ StereoCalibrator::StereoCalibrator(LaserController* laser,
 
 StereoCalibrator::~StereoCalibrator(){}
 
-void StereoCalibrator::calibrateCameras(cv::Mat &frame, const uint id)
+void StereoCalibrator::calibrateCameras(cv::Mat &frame, const int id)
 {
     // Assess enough time between two frame captures
     const auto now = std::chrono::high_resolution_clock::now();
-    if (now-m_last_frame_captured[id]<m_min_capture_delay) {return;}
+    if (now-m_last_frame_captured[id]<m_min_capture_delay) { return; }
     m_last_frame_captured[id] = now;
 
     // Extract checkerboard corner points
     if (m_intrinsic_calib_image_points[id].size()<m_min_frames_to_capture)
     {
         std::vector<cv::Point2f> points;
-        bool ret = cv::findChessboardCorners(frame,
-                                             m_pattern_size,
-                                             points);
+        bool ret = cv::findChessboardCorners(frame, m_pattern_size, points);
         if (ret)
         {
             cv::cornerSubPix(frame,points,cv::Size(5,5),cv::Size(-1,-1),
@@ -49,8 +46,8 @@ void StereoCalibrator::calibrateCameras(cv::Mat &frame, const uint id)
                              + cv::TermCriteria::EPS,30, DBL_EPSILON));
             m_intrinsic_calib_image_points[id].push_back(points);
             cv::drawChessboardCorners(frame,m_pattern_size,points,ret);
-            printf("Calibrating camera %d: %d frames retrieved. \n\r",id,
-                   m_intrinsic_calib_image_points[id].size());
+            printf("Calibrating camera %d: %d frames retrieved. \n\r",
+                   id, m_intrinsic_calib_image_points[id].size());
         }
     }
 
@@ -89,7 +86,7 @@ void StereoCalibrator::calibrateCameras(cv::Mat &frame, const uint id)
                                              60, DBL_EPSILON));
 
         // Extract extrinsics
-        double rms = cv::stereoCalibrate(
+        float rms = cv::stereoCalibrate(
             worldPoints1,
             m_intrinsic_calib_image_points[0],
             m_intrinsic_calib_image_points[1],
@@ -115,7 +112,7 @@ void StereoCalibrator::calibrateCameras(cv::Mat &frame, const uint id)
 
 void StereoCalibrator::calibrateLaser(cv::Mat& frame0,
                                       cv::Mat& frame1,
-                                      const uint id)
+                                      const int id)
 {   
     // Reset laser pos
     m_laser->setPos(m_laser->m_max_x/2,m_laser->m_max_y/2);
@@ -231,7 +228,7 @@ void StereoCalibrator::calibrateLaser(cv::Mat& frame0,
                                              60, DBL_EPSILON));
 
         // Extract extrinsics (camera0-camera1)
-        double rms0 = cv::stereoCalibrate(
+        float rms0 = cv::stereoCalibrate(
             worldPoints0,
             m_intrinsic_calib_image_points[0],
             m_intrinsic_calib_image_points[1],
@@ -246,7 +243,7 @@ void StereoCalibrator::calibrateLaser(cv::Mat& frame0,
                              DBL_EPSILON));
 
         // Extract extrinsics (Camera0-Laser)
-        double rms1 = cv::stereoCalibrate(
+        float rms1 = cv::stereoCalibrate(
             worldPoints1,
             m_intrinsic_calib_image_points[0],
             m_intrinsic_calib_laser_points,
@@ -261,7 +258,7 @@ void StereoCalibrator::calibrateLaser(cv::Mat& frame0,
                              DBL_EPSILON));
 
         // Extract extrinsics (Laser-Camera1)
-        double rms2 = cv::stereoCalibrate(
+        float rms2 = cv::stereoCalibrate(
             worldPoints2,
             m_intrinsic_calib_image_points[1],
             m_intrinsic_calib_laser_points,
@@ -310,10 +307,6 @@ void StereoCalibrator::saveCamerasCalibration()
 void StereoCalibrator::saveLaserCalibration()
 {
     cv::FileStorage fs(m_path_calib_laser, cv::FileStorage::WRITE);
-    //fs << "camera_matrix0" << m_camera_intrinsics[0].m_K;
-    //fs << "dist_coeffs0" << m_camera_intrinsics[0].m_D;
-    //fs << "camera_matrix1" << m_camera_intrinsics[1].m_K;
-    //fs << "dist_coeffs1" << m_camera_intrinsics[1].m_D;
     fs << "camera_matrix_laser" << m_camera_intrinsics[2].m_K;
     fs << "dist_coeffs_laser" << m_camera_intrinsics[2].m_D;
     fs << "R1" << m_R[1];
@@ -337,8 +330,10 @@ const std::vector<cv::Point3f> StereoCalibrator::calculateWorldPoints()
     std::vector<cv::Point3f> points;
     points.reserve(static_cast<uint64_t>(m_pattern_size.x * m_pattern_size.y));
 
-    for (int i = 0; i < m_pattern_size.y; i++) {
-        for (int j = 0; j < m_pattern_size.x; j++) {
+    for (int i = 0; i < m_pattern_size.y; i++)
+    {
+        for (int j = 0; j < m_pattern_size.x; j++)
+        {
             points.emplace_back(cv::Point3f(j * m_pattern_square_size,
                                             i * m_pattern_square_size, 0));
         }
@@ -346,7 +341,7 @@ const std::vector<cv::Point3f> StereoCalibrator::calculateWorldPoints()
     return points;
 }
 
-void StereoCalibrator::pointLaserToPixel(const int rGoal, const int cGoal, const uint id)
+void StereoCalibrator::pointLaserToPixel(const int rGoal, const int cGoal, const int id)
 {
     int r = m_filter[id]->getX();
     int c = m_filter[id]->getY();
@@ -368,10 +363,7 @@ void StereoCalibrator::pointLaserToPixel(const int rGoal, const int cGoal, const
 
         // Update laser command
         int dx;
-        if (m_learningRate*diff[0]>m_step)
-        {
-            dx = m_learningRate*diff[0];
-        }
+        if (m_learningRate*diff[0]>m_step) { dx = m_learningRate*diff[0]; }
         else { dx = (diff[0]>0?m_step:-m_step); }
 
         int dy;
@@ -388,9 +380,110 @@ void StereoCalibrator::pointLaserToPixel(const int rGoal, const int cGoal, const
 
     //=== DEBUG ===//
     printf("Current laser position (%d,%d). \n\r",r,c);
-    printf("Diff: (%d,%d) - MSE: %d. \n\r",
-           diff[0],diff[1],mse);
+    printf("Diff: (%d,%d) - MSE: %d. \n\r", diff[0],diff[1],mse);
     printf("Laser commands: (%d,%d). \n\r",m_laser->getX(),m_laser->getY());
     //=== END DEBUG ===//
     return;
+}
+
+
+DLT::DLT()
+{
+    m_U.resize(3*3);
+    m_T.resize(4*4);
+}
+
+DLT::~DLT() {}
+
+void DLT::normalizePoints(cv::Mat& points2D,
+                          cv::Mat& points3D)
+{
+    // Compute mean and std of points array
+    cv::Scalar mean2D, mean3D, sigma2D, sigma3D;
+    cv::meanStdDev(points2D,mean2D,sigma2D);
+    cv::meanStdDev(points3D,mean3D,sigma3D);
+
+    // Compute normalization matrices
+    cv::Scalar s_2D = sigma2D/std::sqrt(2);
+    cv::Mat U = (cv::Mat_<double>(3,3) << s_2D[0],       0, mean2D[0],
+                                                0, s_2D[1], mean2D[1],
+                                                0,       0,         1);
+    cv::invert(U,m_U);
+
+    cv::Scalar s_3D = sigma3D/std::sqrt(3);
+    cv::Mat T = (cv::Mat_<double>(4,4) << s_3D[0],       0,       0, mean3D[0],
+                                                0, s_3D[1],       0, mean3D[1],
+                                                0,       0, s_3D[2], mean3D[2],
+                                                0,       0,       0,         1);
+    cv::invert(T,m_T);
+
+    // Normalize points
+    //points2D = m_U * points2D;
+    //points3D = m_T * points3D;
+}
+
+
+// Code from https://github.com/lagadic/camera_localization/blob/master/opencv/pose-basis/pose-dlt-opencv.cpp
+void DLT::extractProjectionMatrix(const std::vector<cv::Point3d>& wX,
+                                  const std::vector<cv::Point2d>& x,
+                                  cv::Mat& ctw, cv::Mat& cRw)
+{
+    int nbpoints = (int)wX.size();
+    cv::Mat A(2*nbpoints, 12, CV_64F, cv::Scalar(0));
+    for (int i=0; i<nbpoints; i++)
+    {
+        A.at<double>(2*i, 0) = wX[i].x;
+        A.at<double>(2*i, 1) = wX[i].y;
+        A.at<double>(2*i, 2) = wX[i].z;
+        A.at<double>(2*i, 3) = 1 ;
+
+        A.at<double>(2*i+1, 4) = wX[i].x;
+        A.at<double>(2*i+1, 5) = wX[i].y;
+        A.at<double>(2*i+1, 6) = wX[i].z;
+        A.at<double>(2*i+1, 7) = 1 ;
+
+        A.at<double>(2*i, 8)  = -x[i].x * wX[i].x;
+        A.at<double>(2*i, 9)  = -x[i].x * wX[i].y;
+        A.at<double>(2*i, 10) = -x[i].x * wX[i].z;
+        A.at<double>(2*i, 11) = -x[i].x;
+
+        A.at<double>(2*i+1, 8)  = -x[i].y * wX[i].x;
+        A.at<double>(2*i+1, 9)  = -x[i].y * wX[i].y;
+        A.at<double>(2*i+1, 10) = -x[i].y * wX[i].z;
+        A.at<double>(2*i+1, 11) = -x[i].y;
+    }
+
+    // Apply SVD on A
+    cv::Mat S, U, Vt;
+    cv::SVD::compute(A, S, U, Vt);
+
+    // Find smallest eigenvalue in S
+    double smallestSv = S.at<double>(0, 0);
+    unsigned int indexSmallestSv = 0 ;
+    for (int i = 1; i < S.rows; i++)
+    {
+        if ((S.at<double>(i, 0) < smallestSv) )
+        {
+            smallestSv = S.at<double>(i, 0);
+            indexSmallestSv = i;
+        }
+    }
+
+    // Right null vector extraction
+    cv::Mat rnv = Vt.row(indexSmallestSv);
+    if (rnv.at<double>(0, 11) < 0) // tz < 0
+        rnv *=-1;
+
+    // Normalization to ensure that ||r3|| = 1
+    double norm = sqrt(   rnv.at<double>(0,8)*rnv.at<double>(0,8)
+                        + rnv.at<double>(0,9)*rnv.at<double>(0,9)
+                        + rnv.at<double>(0,10)*rnv.at<double>(0,10));
+    rnv /= norm;
+
+    // Extract translation vector and rotation matrix
+    for (int i = 0 ; i < 3 ; i++) {
+        ctw.at<double>(i,0) = rnv.at<double>(0, 4*i+3); // Translation
+        for (int j = 0 ; j < 3 ; j++)
+          cRw.at<double>(i,j) = rnv.at<double>(0, 4*i+j); // Rotation
+    }
 }
